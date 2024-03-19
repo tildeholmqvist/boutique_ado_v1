c5a4a8472870f0e5c90011b18260f03f3e0b977d
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-03-19 10:21:39 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -3,17 +3,19 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
-# Create your views here.
+
 def checkout(request):
     bag = request.session.get('bag', {})
-    if not bag: 
+    if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OvzS2E3EGhvy4O1AwSnHcFL1GdvMPGvAeybtS0hI87HUvZhB5ijUvmvLXSAxfKtex1O79avSFGdXfmwI6ewJHvj00R9WJs5XD',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
